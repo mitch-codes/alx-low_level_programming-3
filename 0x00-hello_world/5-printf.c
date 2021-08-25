f46714fc532e077ba234a8186c4b641e7e380a10
AlexWambui <alexwambuik@gmail.com>
2021-08-25 15:08:51 -0700

    got rid of whitespace after the string and added a new comment

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,11 +2,12 @@
 
 /**
  *main:void - starts a file
+ *void - argument for main function
  *return:0 - ends the program with 0
 */
 
 int main(void)
 {
-printf("%s", "with proper grammar, but the outcome is a piece of art, \n");
+printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
