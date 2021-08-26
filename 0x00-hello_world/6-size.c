66d6696098604ee0d8d3015e4f630457b2b96833
AlexWambui <alexwambuik@gmail.com>
2021-08-26 12:13:05 -0700

    adjusted the output section for types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,11 +7,11 @@
 
 int main(void)
 {
-printf("Size of char: %1d byte(s)\n", sizeof(char));
-printf("Size of int: %1d byte(s)\n", sizeof(int));
-printf("Size of long: %1d byte(s)\n", sizeof(long int));
-printf("Size of long long int: %1d byte(s)\n", sizeof(long long int));
-printf("Size of float: %1d byte(s)\n", sizeof(float));
+printf("Size of char: %d byte(s)\n", sizeof(char));
+printf("Size of int: %d byte(s)\n", sizeof(int));
+printf("Size of long: %d byte(s)\n", sizeof(long int));
+printf("Size of long long int: %d byte(s)\n", sizeof(long long int));
+printf("Size of float: 1d byte(s)\n", sizeof(float));
 return (0);
 }
  
