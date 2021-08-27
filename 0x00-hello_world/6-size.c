3ae84c7f123aa704053cf71a4bd613bf77d17955
AlexWambui <alexwambuik@gmail.com>
2021-08-27 02:05:38 -0700

    removed the whitespace and corrected spelling on line 9 for bytes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,8 +5,8 @@
 *Return: Always 0 (Success)
 */
 int main(void)
-{	
-	printf("Size of a char: %d btyes(s)\n", sizeof(char));
+{
+	printf("Size of a char: %d bytes(s)\n", sizeof(char));
 	printf("Size of an int: %d bytes(s)\n", sizeof(int));
 	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
