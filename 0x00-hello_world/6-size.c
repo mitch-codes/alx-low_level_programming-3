85084099f740aec46158bb2e842886a2a1efdc34
AlexWambui <alexwambuik@gmail.com>
2021-08-27 01:36:10 -0700

    tried the betty coding style

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,15 @@
 */
 int main(void)
 {
-printf("Size of a char: %d byte(s)\n", sizeof(char));
-printf("Size of an int: %d byte(s)\n", sizeof(int));
-printf("Size of a long int: %d byte(s)\n", sizeof(long int));
-printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
-printf("Size of a float: %d byte(s)\n", sizeof(float));
-return (0);
+	char c;
+	int i;
+	long int li;
+	long long int lli;
+	float f;
+	printf("Size of a char: %d btyes(s)\n", sizeof(c));
+	printf("Size of an int: %d bytes(s)\n", sizeof(i));
+	printf("Size of a long int: %d byte(s)\n", sizeof(li));
+	printf("Size of a long long int: %d byte(s)", sizeof(lli));
+	printf("Size of a float: %d byte(s)", sizeof(f));
+	return (0);
 }
