f6bac6df3301dd17badedc08d0191f875c7b31fe
AlexWambui <alexwambuik@gmail.com>
2021-08-27 09:59:26 -0700

    debugging: added white space after if and used curly braces

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -29,8 +29,10 @@ int main(void)
 						putchar(' ');
 						putchar(e);
 						putchar(i);
-						if(h == 57 && g == 56 && e == 57 && i == 57)
+						if (h == 57 && g == 56 && e == 57 && i == 57)
+						{
 							break;
+						}
 						putchar(',');
 						putchar(' ');
 					}
