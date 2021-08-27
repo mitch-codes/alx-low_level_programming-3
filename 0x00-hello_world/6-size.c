2e5907ddc03baecbfff873a136c84020e9cec489
AlexWambui <alexwambuik@gmail.com>
2021-08-27 01:43:40 -0700

    added a new line after declarations

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,6 +11,7 @@ int main(void)
 	long int li;
 	long long int lli;
 	float f;
+	
 	printf("Size of a char: %d btyes(s)\n", sizeof(c));
 	printf("Size of an int: %d bytes(s)\n", sizeof(i));
 	printf("Size of a long int: %d byte(s)\n", sizeof(li));
