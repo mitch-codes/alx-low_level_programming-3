db0d53cde58df27422b680adf40ee838f2778838
AlexWambui <alexwambuik@gmail.com>
2021-08-27 02:54:31 -0700

    debugging: changed the condition for the for loop

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,7 +9,7 @@ int main(void)
 	char alphabets[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int i;
 
-	for (i = 0; i < 26; i++)
+	for (i = 0; i < 52; i++)
 		putchar(alphabets[i]);
 	putchar('\n');
 	return (0);
