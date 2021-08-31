d3904a0a8507da4ff487d07b63bbda6b6631e381
AlexWambui <alexwambuik@gmail.com>
2021-08-31 04:09:54 -0700

    debugging

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -5,10 +5,10 @@
 */
 int main(void)
 {
-	char school[10] = "Holberton";
+	char school[10] = "putchar";
 	int i;
 
-	for (i = 0; i < 9; i++)
+	for (i = 0; i < 7; i++)
 		_putchar(school[i]);
 	_putchar('\n');
 	return (0);
