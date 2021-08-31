e72be486e59a249a63f1f49f5e111162d25820e8
AlexWambui <alexwambuik@gmail.com>
2021-08-31 01:41:37 -0700

    print alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,14 @@
+#include "functions.h"
+/**
+ * print_alphabet_x10 - prints alphabet 10 times
+ * Return - Always 0 (Success)
+*/
+void print_alphabet_x10(void)
+{
+	int x, y;
+
+	for (x = 0; x <= 9; x++)
+		for (y = 97; b<= 122; b++)
+			_putchar(y);
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/start.c
@@ -0,0 +1,8 @@
+#include "functions.h"
+/**
+ * function - prints
+ * Return - Always 0 (Success)
+*/
+void f(void)
+{
+}
