df8db967f7ab838a9cd7d1051dad5c0d034fa1da
AlexWambui <alexwambuik@gmail.com>
2021-08-31 06:42:16 -0700

    debugging

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
  * _isalpha - checks for alphabetic character.
+ * @c: the ASCII character
  * Return: 1 if is a letter, lowercase or uppercase. 0 if otherwise.
 */
 int _isalpha(int c)
