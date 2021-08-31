b488f458a362969410884eedfd887e6ebe982ea9
AlexWambui <alexwambuik@gmail.com>
2021-08-31 01:26:35 -0700

    print holberton then a new line

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - prints Holberton then a new line
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char school[10] = "Holberton";
+	int i = 0;
+	while (i < 9)
+	{
+		_putchar(school[i]);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
