9c99cdd6bf05e31d482cc20d3730bce3f3cf6ce3
AlexWambui <alexwambuik@gmail.com>
2021-08-31 06:37:54 -0700

    debigging

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,12 +1,10 @@
-#include "functions.h"
+#include "main.h"
 /**
  * _isalpha - checks for alphabetic character.
  * Return: 1 if is a letter, lowercase or uppercase. 0 if otherwise.
 */
 int _isalpha(int c)
 {
-	int c;
-
 	if ((c >= 97 && c <= 122) || (c >= 65 <= 90))
 		return (1);
 	else
