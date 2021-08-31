2b2c011e611d1aac0228f1221a302e58e9256772
AlexWambui <alexwambuik@gmail.com>
2021-08-31 06:21:48 -0700

    debugging

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,7 +8,8 @@ void print_alphabet_x10(void)
 	int x, y;
 
 	for (x = 0; x <= 9; x++)
-		for (y = 97; y<= 122; y++)
+		for (y = 97; y <= 122; y++)
 			_putchar(y);
 	_putchar('\n');
+	return (0);
 }
