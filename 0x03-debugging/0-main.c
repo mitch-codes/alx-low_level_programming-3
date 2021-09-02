35b0abfd1636d0a34badcb769ad84be1b831f7a5
AlexWambui <alexwambuik@gmail.com>
2021-09-01 22:53:31 -0700

    added code for 0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * positive_or_negative - checks if function gives the correct output
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i = 0;
+
+	positive_or_negative(i);
+	return (0);
+}
