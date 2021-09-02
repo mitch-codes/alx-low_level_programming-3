aface6400a6ceab3f55cd8dad7cc061c1e15aa42
AlexWambui <alexwambuik@gmail.com>
2021-09-02 07:15:12 -0700

    debugging

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,13 +2,14 @@
 /**
  * print_most_numbers - prints numbers leaving 2 and 4
  * 
- * Return: Always 0
+ * Description: prints 0-9 expect numbers 2 and 4
 */
 
 void print_most_numbers(void)
 {
-	char n = 0;
+	int n;
 
+	n = 0;
 	while (n <= 9)
 	{
 		if (n != 2 && n != 4)
