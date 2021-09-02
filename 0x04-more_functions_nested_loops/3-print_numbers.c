5a7b997f9fda4889634846a6ab7632a48ba42882
AlexWambui <alexwambuik@gmail.com>
2021-09-02 06:15:10 -0700

    prints 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_number - prints 0 to 9
+ * Return: Always 0
+*/
+void print_numbers(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+		_putchar(x);
+	_putchar('\n');
+}
