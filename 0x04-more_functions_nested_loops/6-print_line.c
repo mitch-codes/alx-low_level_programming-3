3c23bed00d66ce8ea513c8be936facfd25b589ff
AlexWambui <alexwambuik@gmail.com>
2021-09-02 08:14:35 -0700

    prints a line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,12 +1,18 @@
 #include "main.h"
 /**
- * xx - xx
- * @c: input value
- * 
- * Description: xx
- * Return: Always 0
+ * print_line - draws a straight line in the terminal
+ * @n: input value
+ * Description: use _putchar to print
 */
-int xx(xx)
+void print_line(int n)
 {
-	xx;
+	int c;
+
+	c = 0;
+	while (c < n)
+	{
+		_putchar('_');
+		c++;
+	}
+	_putchar('\n');
 }
